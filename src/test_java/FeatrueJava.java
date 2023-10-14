691f0fac6ca6cd19c09cf87d02661f67baf57e57
razel551 <kirazzang551@gmail.com>
2023-10-14 20:11:01 +0900

    develop 수정만 입력

--- src/test_java/FeatrueJava.java
+++ src/test_java/FeatrueJava.java
@@ -3,8 +3,6 @@ package test_java;
 public class FeatrueJava {
 
 	public void main() {
-		System.out.println("Feature 수정");
-		System.out.println("Feature 수정");
 		System.out.println("Develope 수정");
 	}
 	
